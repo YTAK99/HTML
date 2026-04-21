a90d1dea0869193672e386f31e15a8e34687e7b0
YTAK99 <157030759+YTAK99@users.noreply.github.com>
2026-04-21 18:25:34 +0900

    Add files via upload

---
+++ 0421/letname.js
@@ -0,0 +1,25 @@
+let name = '제갈철수';
+let welcomeText = '님 반가워요!';
+let resultWelcomeText = name + welcomeText;
+
+console.log(resultWelcomeText);     // 제갈철수님 반가워요!
+
+///////////////////////////////////////////////////////////////
+
+let guestName = '제갈명수';
+let greetingText = `welcome ${guestName}!`;
+
+console.log(greetingText);      // welcome 제갈명수!
+
+
+남궁 선우 황보 독고 
+
+
+let strA = "10";
+let strB = "10개";
+
+let numA = Number(strA);
+let numB = Number(strB);
+
+console.log(numA);
+console.log(numB);
